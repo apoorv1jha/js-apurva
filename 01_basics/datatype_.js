613c920bcefb0b97d8738ccb81cd61a5ee980f36
apoorv1jha <jha77apurva@gmail.com>
2024-10-17 10:17:41 +0000

    datatypes

---
+++ 01_basics/datatype_.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all js code as newer version
+// alert(3+3) //we are using nodejs, not browser
+//code readability should be high
+//ecma pe jaa ke dekho ek baar
+
+let name="apurva"
+let age=18
+let isLoggedIn=false
+let state;
+let state1=null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "apurva");
+
+
+// typeof undefined = undefined
+// type of null = object
---
+++ 01_basics/var_let_const.js
@@ -0,0 +1,15 @@
+const account_id=12435
+let account_email="apurv@gmail.com"
+var account_password="14532"
+account_city="Jaipur"
+let account_state;
+
+// account_id=change is not allowed
+account_email="sonu@gmail.com"
+account_password="1324"
+account_city="darbhanga"
+console.table([account_id,account_email,account_password,account_city,account_state])
+
+
+// prefer not to use var
+// beacuse of issue in block and scope and fuctional scope
