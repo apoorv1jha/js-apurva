b46a8538a2eec4b4c129aa6e85c639a3d639f07d
apoorv1jha <jha77apurva@gmail.com>
2024-10-21 14:31:08 +0000

    conversion of datatype

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,26 @@
+let score ="n"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true=>1 ; anf false =>0
+
+let isLoggedIn = 2
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "apurva o any name" or any number => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
