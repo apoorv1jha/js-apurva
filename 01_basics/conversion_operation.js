a89224bfb43bd80d1d9880aa11ec64c337a4829c
apoorv1jha <jha77apurva@gmail.com>
2024-10-24 16:44:34 +0000

    conversion_operation

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -24,3 +24,51 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
 
+
+
+
+
+
+// **********************************Operations***********************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Apurva"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 1 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log((2+4)*3%5);
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
+
+
+
+
